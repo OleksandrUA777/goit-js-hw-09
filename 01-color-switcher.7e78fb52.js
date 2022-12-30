1f317abb516d7c01416b8b3cf27eb8b06d7d7689
OleksandrUA777 <110254286+OleksandrUA777@users.noreply.github.com>
2022-12-30 13:14:45 +0000

    Deploying to gh-pages from @ OleksandrUA777/goit-js-hw-09@786c9111c9723bc81b4ec07d6c0c8fae4c9ea982 🚀

---
+++ 01-color-switcher.7e78fb52.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(o){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled",""),e.removeAttribute("disabled","")}),1e3)})),e.addEventListener("click",(function(r){clearInterval(d),t.removeAttribute("disabled",""),e.setAttribute("disabled","")}));let d=null;e.setAttribute("disabled","");
+//# sourceMappingURL=01-color-switcher.7e78fb52.js.map
