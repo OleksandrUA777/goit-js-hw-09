786c9111c9723bc81b4ec07d6c0c8fae4c9ea982
Oleksandr <110254286+OleksandrUA777@users.noreply.github.com>
2022-12-30 14:14:19 +0100

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+ const startBtnRef = document.querySelector('button[data-start]')
+ const stopBtnRef = document.querySelector('button[data-stop]')
+ const bodyRef = document.querySelector('body')
+
+ startBtnRef.addEventListener('click',buttonStartClickHandler)
+ stopBtnRef.addEventListener('click',buttonStopClickHandler)
+
+
+let intervalId = null
+stopBtnRef.setAttribute('disabled','')
+
+
+ function buttonStartClickHandler(event){
+     intervalId = setInterval(() =>{
+    bodyRef.style.backgroundColor = getRandomHexColor()
+
+    startBtnRef.setAttribute('disabled','')
+    stopBtnRef.removeAttribute('disabled','')
+    },1000) 
+ }
+ function buttonStopClickHandler(event){
+    clearInterval(intervalId)
+
+    startBtnRef.removeAttribute('disabled','')
+    stopBtnRef.setAttribute('disabled','')
+ }
